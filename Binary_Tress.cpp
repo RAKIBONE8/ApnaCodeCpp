0a35a49f26fe6017cdbfe6ba79bd9c4779a44212
Rakib Mondal <143431829+RAKIBONE8@users.noreply.github.com>
2023-12-10 13:09:43 +0530

    Binary Tree

---
+++ Binary_Tress.cpp
@@ -0,0 +1,84 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    struct Node *left;
+    struct Node *right;
+
+    Node(int val)
+    {
+        data = val;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+void preOrder(struct Node *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+    cout << root->data << " ";
+    preOrder(root->left);
+    preOrder(root->right);
+}
+void inOrder(struct Node *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+    inOrder(root->left);
+    cout << root->data << " ";
+    inOrder(root->right);
+}
+void postOrder(struct Node *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+    postOrder(root->left);
+    postOrder(root->right);
+    cout << root->data << " ";
+}
+
+int main()
+{
+    struct Node *root = new Node(10);
+    root->left = new Node(20);
+    root->right = new Node(30);
+    root->left->left = new Node(40);
+    root->left->right = new Node(50);
+    root->right->left = new Node(60);
+    root->right->right = new Node(70);
+
+    /*
+
+            10
+         /      \
+        20       30
+       /  \     /   \
+     40    50  60   70
+
+    */
+
+    cout << "PreOrder :";
+    preOrder(root);
+    cout << endl
+         << endl;
+
+    cout << "InOrder :";
+    inOrder(root);
+    cout << endl
+         << endl;
+
+    cout << "PostOrder :";
+    postOrder(root);
+    cout << endl
+         << endl;
+    return 0;
+}
